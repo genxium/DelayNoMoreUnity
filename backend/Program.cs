23f88e35e00a9e0515690bd63e3e0fd2f39467ca
yflu <genxium@hotmail>
2023-03-22 23:16:27 +0800

    Added websocket server strawman codes.

--- backend/Program.cs
+++ backend/Program.cs
@@ -3,19 +3,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// Reference https://learn.microsoft.com/en-us/aspnet/core/fundamentals/websockets?view=aspnetcore-7.0 -- just ignore the SignalR part of this page which is pure noise, this official document points me to the sample codebase at https://github.com/dotnet/AspNetCore.Docs/tree/main/aspnetcore/fundamentals/websockets/samples which is by far the most compact sample of C# Websocket server without SignalR!
+var webSocketOptions = new WebSocketOptions {
+    KeepAliveInterval = TimeSpan.FromMinutes(2)
+};
+app.UseWebSockets(webSocketOptions);
 
-app.UseAuthorization();
+// The following 2 lines make "app" use "wwwroot" as the default web root for serving static file, e.g. "index.html". 
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.MapControllers();
 
---
+++ backend/README.md
@@ -0,0 +1,7 @@
+Instead of running the backend in an IDE, e.g. Visual Studio, it's more convenient to run it by just 
+
+```
+backend> dotnet run
+```
+
+for testing.
