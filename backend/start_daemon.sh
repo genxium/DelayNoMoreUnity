eb2b5ecaeda57bc14679c5cd0e5684d3582bef6d
genxium <genxium@unknown.com>
2023-04-13 17:29:20 +0800

    Fixes for backend scripts.

--- backend/start_daemon.sh
+++ backend/start_daemon.sh
@@ -9,7 +9,7 @@ basedir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 OS_USER=$USER
 ServerEnv=$1
-EXEPATH=bin/${1}/net7.0/backend.exe
+EXEPATH=bin/$1/net7.0/backend
 LOG_PATH="/var/log/dllm.log"
 
 # Make sure that the following "PidFile" is "git ignored".
@@ -18,5 +18,11 @@ PID_FILE="$basedir/dllm.pid"
 sudo su - root -c "touch $LOG_PATH" 
 sudo su - root -c "chown $OS_USER:$OS_USER $LOG_PATH" 
 
-$basedir/$EXEPATH >$LOG_PATH 2>&1 &
+if [ "Debug" == $ServerEnv ]; then
+        dotnet run >$LOG_PATH 2>&1 &
+elif [ "Release" == $ServerEnv ]; then
+        $basedir/$EXEPATH >$LOG_PATH 2>&1 &
+else
+        echo "Only Debug or Release run is supported!"
+fi
 echo $! > $PID_FILE
