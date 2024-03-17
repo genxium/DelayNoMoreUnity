8fb47dfb84f9d2021f32fc23de9d1736d251b0ec
genxium <genxium@hotmail.com>
2024-03-17 23:12:19 +0800

    Added background in SmallForest.

---
+++ frontend/Assets/Resources/Tiled/shared_comps/bkg-shrinked/mountain4-bkg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="mountain4-bkg" tilewidth="64" tileheight="22" tilecount="16" columns="4">
+ <image source="mountain4-bkg.png" width="256" height="88"/>
+</tileset>
