89cc46cb5691137f707886a2d5fda7e05829a9d8
genxium <genxium@hotmail.com>
2024-03-18 16:58:57 +0800

    Added colorpad for better background coloring.

---
+++ frontend/Assets/Resources/Tiled/shared_comps/bkg-shrinked/colorpad-bkg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="colorpad-bkg" tilewidth="16" tileheight="16" tilecount="8" columns="4">
+ <image source="colorpad-bkg.png" width="64" height="32"/>
+</tileset>
