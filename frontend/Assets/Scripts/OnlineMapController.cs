23d4737c1ed3224578f958b443e2112b52f4b38e
genxium <genxium@hotmail.com>
2024-05-29 13:19:01 +0800

    Fixed peer holepunching timing.

--- frontend/Assets/Scripts/OnlineMapController.cs
+++ frontend/Assets/Scripts/OnlineMapController.cs
@@ -140,7 +140,6 @@ public class OnlineMapController : AbstractMapController {
                     var newPeerUdpAddrList = wsRespHolder.PeerUdpAddrList;
                     Debug.Log(String.Format("Handling DOWNSYNC_MSG_ACT_PEER_UDP_ADDR in main thread, newPeerUdpAddrList: {0}", newPeerUdpAddrList));
                     UdpSessionManager.Instance.UpdatePeerAddr(roomCapacity, selfPlayerInfo.JoinIndex, newPeerUdpAddrList);
-                    UdpSessionManager.Instance.PunchAllPeers(wsCancellationToken);
                     break;
                 case DOWNSYNC_MSG_ACT_BATTLE_READY_TO_START:
                     /*
@@ -148,6 +147,8 @@ public class OnlineMapController : AbstractMapController {
                     
                     In practice, I found a weird case where P2 starts holepunching P1 much earlier than the opposite direction (e.g. when P2 joins the room later, but gets the peer udp addr of P1 earlier upon DOWNSYNC_MSG_ACT_BATTLE_COLLIDER_INFO), the punching for both directions would fail if the firewall(of network provider) of P1 rejected & blacklisted the early holepunching packet from P2 for a short period (e.g. 1 minute).
                      */
+                    UdpSessionManager.Instance.PunchAllPeers(wsCancellationToken);
+
                     var speciesIdList = new int[roomCapacity];
                     for (int i = 0; i < roomCapacity; i++) {
                         speciesIdList[i] = wsRespHolder.Rdf.PlayersArr[i].SpeciesId;
